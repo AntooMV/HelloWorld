f34fa92b092e7cfdb3f397b6b6795729c556cbb7
AntooMV <73442818+AntooMV@users.noreply.github.com>
2022-10-16 19:34:40 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// Prints some shit
-		System.out.println("Hello world, this is my first proyect");
+		System.out.println("Hello world, this is my first project");
 	}
 }
